c830baae0d6ecbb541ffe8a0da623ffdb43820e3
moldovanevelin <moldovan.evelin.b@gmail.com>
2023-03-28 12:24:27 +0200

    ws3

---
+++ Workshop_03/Workshop_03/ProgressBarColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Workshop_03
+{
+    public class ProgressBarColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double progressv = (double)value;
+            Brush color = Brushes.Green;
+
+            if (progressv <= 6 && progressv > 3)
+            {
+                color = Brushes.Yellow;
+            }
+            else if (progressv <= 3)
+            {
+                color = Brushes.Red;
+            }
+            return color;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Workshop_03/Workshop_03/Soldier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_03
+{
+    public class Soldier
+    {
+        private string type;
+        private int power;
+        private int vitality;
+        private int value;
+        public Soldier(string type, int power, int vitality, int value)
+        {
+            this.type = type;
+            this.power = power;
+            this.vitality = vitality;
+            this.value = value;
+        }
+        public string Type { get { return type; } set { type = value; } }
+        public int Power { get { return power; } set { power = value; } }
+        public int Vitality { get { return vitality; } set { vitality = value; } }
+        public int Value { get { return value; } set { this.value = value; } }
+
+    }
+}
