0bf954afc3a0dfae0108a170bf0606bca0b9f207
moldovanevelin <moldovan.evelin.b@gmail.com>
2023-03-28 14:52:10 +0200

    Remove and Add Click are working

--- Workshop_03/Workshop_03/MainWindow.xaml.cs
+++ Workshop_03/Workshop_03/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,24 +21,60 @@ namespace Workshop_03
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Soldier> army;
+        public int money { get; set; }       
         public MainWindow()
         {
             InitializeComponent();
+                  
+            money = 0;
+
+            army = new ObservableCollection<Soldier>() { };           
+            army.Add(new Soldier() { Type = "marine", Power = 6, Vitality = 8, Value = 7 });
+            army.Add(new Soldier() { Type = "pilot", Power = 7, Vitality = 6, Value = 9 });
+            army.Add(new Soldier() { Type = "sniper", Power = 10, Vitality = 5, Value = 10 });
+            army.Add(new Soldier() { Type = "engineer", Power = 8, Vitality = 4, Value = 3 });
+            army.Add(new Soldier() { Type = "veteran", Power = 5, Vitality = 5, Value = 2 });
+            lbox_left.ItemsSource = army;
+            lb_cost.Content = $"Money: {money}";           
         }
 
         private void b_add_Click(object sender, RoutedEventArgs e)
         {
-
+           if (lbox_left.SelectedItem!=null && lbox_left.SelectedItem is Soldier s)
+           {
+                lb_right.Items.Add(new string($"{s.Type} {s.Power} {s.Vitality} {s.Value}"));
+                lb_cost.Content = $"Money: {money+=s.Value}";
+           }
+           if (lbox_left.SelectedItem == null)
+           {
+               MessageBox.Show("Choose a soldier!");
+           }
         }
 
         private void b_remove_Click(object sender, RoutedEventArgs e)
         {
-
+            if (lbox_left.SelectedItem != null && lbox_left.SelectedItem is Soldier s)
+            {
+                lb_right.Items.Remove(new string($"{s.Type} {s.Power} {s.Vitality} {s.Value}"));
+                if ((money-s.Value)>=0)
+                {
+                    lb_cost.Content = $"Money: {money -= s.Value}";
+                }
+            }
+            if (lbox_left.SelectedItem == null)
+            {
+                MessageBox.Show("Choose a soldier!");
+            }
         }
 
         private void b_edit_Click(object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Soldier s)
+            {
+                EditTrooper et = new EditTrooper();
+                et.Show();
+            }
         }
 
     }
--- Workshop_03/Workshop_03/Soldier.cs
+++ Workshop_03/Workshop_03/Soldier.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Workshop_03
 {
-    public class Soldier
+    public class Soldier: INotifyPropertyChanged
     {
         private string type;
         private int power;
         private int vitality;
         private int value;
-        public Soldier(string type, int power, int vitality, int value)
-        {
-            this.type = type;
-            this.power = power;
-            this.vitality = vitality;
-            this.value = value;
-        }
-        public string Type { get { return type; } set { type = value; } }
-        public int Power { get { return power; } set { power = value; } }
-        public int Vitality { get { return vitality; } set { vitality = value; } }
-        public int Value { get { return value; } set { this.value = value; } }
+      
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler !=null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public string Type { get { return type; } set { type = value; OnPropertyChanged(); } }
+        public int Power { get { return power; } set { power = value; OnPropertyChanged(); } }
+        public int Vitality { get { return vitality; } set { vitality = value; OnPropertyChanged(); } }
+        public int Value { get { return value; } set { this.value = value; OnPropertyChanged(); } }
+
+       
     }
 }
